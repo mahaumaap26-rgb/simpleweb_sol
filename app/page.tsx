5a97a51d47329f2d1a958a80ddaa0f51cb2c68c8
mahaumaap26-rgb <mahaumaap26@gmail.com>
2026-07-04 23:21:31 +0400

    Rename page.tsx to app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,1 @@
+export default function Home(){return <main><h1>Replace with your components</h1></main>}
--- page.tsx
+++
@@ -1,1 +0,0 @@
-export default function Home(){return <main><h1>Replace with your components</h1></main>}
